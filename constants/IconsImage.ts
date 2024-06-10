676abd4ebd26ce973d8c80a86cc1e78790a0f396
RudyProg <sanlager@yandex.ru>
2024-06-10 10:10:32 +0700

    first

---
+++ constants/IconsImage.ts
@@ -0,0 +1,9 @@
+const BusImage = require("@/assets/images/iconsNavigate/buslight.png");
+const CameraImage = require("@/assets/images/iconsNavigate/camerasecuritylight.png");
+const TempImage = require("@/assets/images/iconsNavigate/temperaturelight.png");
+const RadioImage = require("@/assets/images/iconsNavigate/heatlight.png");
+
+export default function IconsImage{
+    const BusImg = BusImage;
+
+};
